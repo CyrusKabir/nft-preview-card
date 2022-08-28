b4c9793b65e0e8946ef572905eb3f85516113f1a
Cyruskabir <ariansepahi@yahoo.com>
2022-08-28 17:16:53 +0430

    General: remove clean-webpack-plugin
    add clean property in output webpack.prod.js (webpack 5 feature)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,17 @@
 const path = require("path");
 const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
     mode: "production",
     output: {
       filename: "[name].[contentHash].bundle.js",
-      path: path.resolve(__dirname, "dist")
+      path: path.resolve(__dirname, "dist"),
+      clean: true
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
-      new CleanWebpackPlugin()
-    ],
+      new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" })],
     module: {
       rules: [
         {
